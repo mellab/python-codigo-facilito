9608f0b2d77f335f1581f71762163e871a5dba6f
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-09-15 06:28:06 +0000

    Numbers

---
+++ numbers.py
@@ -0,0 +1,20 @@
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print("Suma", result)
+
+result = number_one - number_two
+print("Resta", result)
+
+result = number_one * number_two
+print("Multiplicación", result)
+
+result = number_one / number_two #Regresa un número flotante
+print("División", result)
+
+result = number_one // number_two #Regresa un número entero
+print("División", result)
+
+result = number_one ** number_two #Regresa un número a su exponencial
+print("Exponencial", result)
