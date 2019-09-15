ca2312584821eca8388e3bd9f08156c3cc5c2937
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-09-15 06:28:29 +0000

    Strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+my_string = "Hola Mundo! 'Eduardo'"
+my_string = """Este es un string que contiene\nsaltos de linea, como puedes ver.\nAdios!"""
+
+course = "Python 3"
+name = "Eduardo"
+
+final_message = "Nuevo curso de "+ course + " por " + name #1
+final_message = "Nuevo curso de %s por %s" %(course, name) #2
+final_message = "Nuevo curso de {} por {}".format(course, name) #3
+
+#print(my_string)
+print(final_message)
