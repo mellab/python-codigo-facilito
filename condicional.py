6d044bbcf60ca278e5d527998231523d4948c9e7
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-09-16 07:40:16 +0000

    Conditionals

---
+++ condicional.py
@@ -0,0 +1,32 @@
+fruta = 'kiwi'
+fruta = 'manzana'
+
+"""if fruta es igual a 'kiwi'""" #Pseudocódigo
+
+if fruta != 'kiwis' : #Si y solo si """>   <   >=   <=   !=   ===
+    print("Son diferentes")
+
+elif fruta == 'manzana':
+    print("Es una manzana")
+
+elif fruta == 'manzana2':
+    pass
+
+elif fruta == 'manzana2':
+    pass
+else:
+    print("No son iguales")
+
+#True = 1
+#False = 0
+
+valor = 0
+valor_dos = 20
+
+if valor or valor_dos > 20 : # [], (), {}, 0, None, '' """Falsos"""
+     print('Es verdad')
+else:
+    print('No es verdad')
+
+#mensaje = "El valor es kiwi" if fruta == 'kiwi' else "No son iguales"
+#print (mensaje)
